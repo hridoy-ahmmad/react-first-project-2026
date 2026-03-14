891ed27220ac2925bf43dcd04e515eaeca180d33
hridoy-ahmmad <hridoyp338y@gmail.com>
2026-03-14 17:46:51 +0600

    Created nav

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,6 @@
 
 import './App.css'
+import Footer from './components/Footer'
 import Nav from './components/Nav'
 
 function App() {
@@ -7,6 +8,7 @@ function App() {
   return (
     <>
       <Nav></Nav>
+      <Footer></Footer>
     </>
   )
 }
